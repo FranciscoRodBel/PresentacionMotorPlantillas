fb89b42d05c4d278a1a04417fdb493eb700d78b6
Jorge Muñoz García <91768274+JorgeBackendProjects@users.noreply.github.com>
2024-02-15 20:21:10 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
     require_once 'vendor/autoload.php';
 
-    // Indicamos en Twig la ubicación de las plantillas
+    //Indicamos en Twig la ubicación de las plantillas
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
 
-    // Definimos las variables que deseamos rellenar en las plantillas
+    //Definimos las variables que deseamos rellenar en las plantillas
     $variablesEmail = [
         'titulo' => 'Ejemplo Twig',
-        'nombre' => 'Cid',
-        'email' => 'cid@campeador.vlc',
-        'asunto' => 'Reconquista'
+        'nombre' => 'CIFP Ceuta',
+        'email' => 'cifpceuta@info.com',
+        'asunto' => 'Actualización para uso de Twig'
     ];
 
-    // Renderizamos con la plantilla 'contacto.html'
+    //Renderizamos con la plantilla 'index.html.twig'
     $plantillaHTML = $twig->render('index.html.twig', $variablesEmail);
 
-    // Mostramos el resultado
+    //Mostramos el resultado
     echo $plantillaHTML;
 ?>
